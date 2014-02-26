3ef85c591e4437e14a9e209e9a420ef212975f6d
Lytsing Huang <hlqing@gmail.com>
2014-02-26 23:44:50 +0800

    proto file rename

--- proto/make_proto.sh
+++ proto/make_proto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-protoc --cpp_out ../ iboxpay.payment.proto
-protoc --python_out=. cup.payment.proto
+protoc --cpp_out ../ iboxpay_payment.proto cup_payment.proto
+protoc --java_out=. iboxpay_payment.proto cup_payment.proto
+protoc --python_out=. cup_payment.proto
 

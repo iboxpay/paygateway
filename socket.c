96b78ab29f4116167352898288560a431a7fadac
Lytsing Huang <hlqing@gmail.com>
2014-02-26 11:19:45 +0800

    add print_headers for socket.c

--- socket.c
+++ socket.c
@@ -26,7 +26,8 @@
 #include "socket.h"
 
 #define PAYMENT_PORT 8000
-#define PAYMENT_SERVER "0.0.0.0"
+#define PAYMENT_SERVER "127.0.0.1"
+static char *server_name = "PayGateway/1.0.0 (Unix)";
 
 struct payment_context {
     const char* payment_contents;
@@ -34,18 +35,63 @@ struct payment_context {
     int recved;
 };
 
-static void backend_cb(struct bufferevent *bev, void *ptr) {
-    //evhtp_request_t * frontend_req = (evhtp_request_t *)ptr;
+static const char * method_strmap[] = {
+    "GET",
+    "HEAD",
+    "POST",
+    "PUT",
+    "DELETE",
+    "MKCOL",
+    "COPY",
+    "MOVE",
+    "OPTIONS",
+    "PROPFIND",
+    "PROPATCH",
+    "LOCK",
+    "UNLOCK",
+    "TRACE",
+    "CONNECT",
+    "PATCH",
+    "UNKNOWN",
+};
 
-    struct evbuffer *input = bufferevent_get_input(bev);
-    //evbuffer_prepend_buffer(frontend_req->buffer_out, input);
+static int print_headers(evhtp_header_t * header, void * arg);
 
-    printf("backend_cb");
-    //evhtp_send_reply(frontend_req, EVHTP_RES_OK);
-    //evhtp_request_resume(frontend_req);
+static void backend_cb(struct bufferevent* bev, void* ctx) {
+    evhtp_request_t* req = (evhtp_request_t *)ctx;
+
+    const char *uri = req->uri->path->full;
+
+    int req_method = evhtp_request_get_method(req);
+    if (req_method >= 16) {
+        req_method = 16;
+    }
+
+    struct evbuffer* output = bufferevent_get_output(bev);
+    struct evbuffer* input = bufferevent_get_input(bev);
+
+    size_t input_len = evbuffer_get_length(input);
+    printf("input_len: %zu\n", input_len);
+
+    size_t output_len1 = evbuffer_get_length(output);
+    printf("output_len1: %zu\n\n", output_len1);
+
+    evbuffer_add_printf(req->buffer_out, "uri : %s\r\n", uri);
+    evbuffer_add_printf(req->buffer_out, "query : %s\r\n", req->uri->query_raw);
+    evhtp_headers_for_each(req->uri->query, print_headers, req->buffer_out);
+    evbuffer_add_printf(req->buffer_out, "Method : %s\n", method_strmap[req_method]);
+    evhtp_headers_for_each(req->headers_in, print_headers, req->buffer_out);
+
+    puts(">>>");
+
+    evhtp_headers_add_header(req->headers_out, evhtp_header_new("Server", server_name, 0, 0));
+    evhtp_headers_add_header(req->headers_out, evhtp_header_new("Content-Type", "text/plain", 0, 0));
+
+    evhtp_send_reply(req, EVHTP_RES_OK);
+    evhtp_request_resume(req);
 }
 
-static void eventcb(struct bufferevent *bev, short events, void *ptr) {
+static void eventcb(struct bufferevent* bev, short events, void* ctx) {
     if (events & BEV_EVENT_CONNECTED) {
         printf("Connect okay.\n");
     } else if (events & (BEV_EVENT_ERROR|BEV_EVENT_EOF)) {
@@ -69,6 +115,17 @@ void router_request_cb(evhtp_request_t* req, void* arg) {
 
     printf("  Received router request on thread %d... ", thr);
 
+
+    evbuf_t *buf = req->buffer_in;;
+    puts("Input data: <<<");
+    while (evbuffer_get_length(buf)) {
+        int n;
+        char cbuf[128];
+        n = evbuffer_remove(buf, cbuf, sizeof(buf)-1);
+        if (n > 0)
+            (void) fwrite(cbuf, 1, n, stdout);
+    }
+
     /* Pause the router request while we run the backend requests. */
     evhtp_request_pause(req);
 
@@ -87,5 +144,24 @@ void router_request_cb(evhtp_request_t* req, void* arg) {
     event_base_dispatch(base);
 
     printf("Ok.\n");
+}
+
+/**
+ * @brief print_headers It displays all headers and values.
+ *
+ * @param header The header of a request.
+ * @param arg The evbuff you want to store the k-v string.
+ *
+ * @return It always return 1 for success.
+ */
+static int print_headers(evhtp_header_t * header, void * arg)
+{
+    evbuf_t * buf = arg;
+
+    evbuffer_add(buf, header->key, header->klen);
+    evbuffer_add(buf, ": ", 2);
+    evbuffer_add(buf, header->val, header->vlen);
+    evbuffer_add(buf, "\r\n", 2);
+    return 1;
 }
 

40b933dd09102ae4c97f650d799f9f202c7ee6e9
Lytsing Huang <hlqing@gmail.com>
2014-02-27 17:35:52 +0800

    move test file into test dir

--- proto/make_proto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-protoc --cpp_out ../ iboxpay_payment.proto cup_payment.proto
-protoc --java_out=. iboxpay_payment.proto cup_payment.proto
-protoc --python_out=. cup_payment.proto
-
---
+++ test/gen_proto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SRC=../proto
+#protoc --cpp_out ../ $SRC/protoc/iboxpay_payment.proto $SRC/cup_payment.proto
+#protoc --java_out=. $SRC/iboxpay_payment.proto $SRC/cup_payment.proto
+protoc --python_out=. -I=$SRC $SRC/cup_payment.proto
